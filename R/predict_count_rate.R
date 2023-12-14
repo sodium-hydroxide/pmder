785a97d0a12a385887f73a8846dc05a65c6c0bbe
sodium-hydroxide <43483259+sodium-hydroxide@users.noreply.github.com>
2023-12-14 09:44:03 -0700

    Testing count_rate

--- R/predict_count_rate.R
+++ R/predict_count_rate.R
@@ -1,21 +1,135 @@
+
 #' Title
 #'
 #' @param input_data
 #' @param photon_energy_keV
 #' @param yield
 #' @param activity_Bq
+#' @param method
+#' @param earth_data
 #' @param cpm
-#' @param net_counts
+#' @param net_counts_rolling
+#' @param net_counts_cumulative
 #' @param count_rate_derivative
-#' @param cumulative_counts
 #' @param collection_time_s
 #' @param speed_cm_s
 #'
-#' @return
 #' @export
 #'
-#' @examples
 predict_count_rate <- function(
+        input_data,
+        photon_energy_keV,
+        yield,
+        activity_Bq = 1,
+        method = "earth",
+        earth_data = earth_model,
+        cpm = FALSE,
+        net_counts_rolling = FALSE,
+        net_counts_cumulative = FALSE,
+        count_rate_derivative = FALSE,
+        collection_time_s = NULL,
+        speed_cm_s = NULL) {
+
+    if (method == "earth") {
+        return(predict_count_rate_earth(
+            input_data,
+            photon_energy_keV,
+            yield,
+            activity_Bq = activity_Bq,
+            earth_data = earth_data,
+            cpm = cpm,
+            net_counts_rolling = net_counts_rolling,
+            net_counts_cumulative = net_counts_cumulative,
+            count_rate_derivative = count_rate_derivative,
+            collection_time_s = collection_time_s,
+            speed_cm_s = speed_cm_s
+        ))
+    } else if(method == "lm") {
+        return(predict_count_rate_lm(
+            input_data,
+            photon_energy_keV,
+            yield,
+            activity_Bq = activity_Bq,
+            cpm = cpm,
+            net_counts_rolling = net_counts_rolling,
+            net_counts_cumulative = net_counts_cumulative,
+            count_rate_derivative = count_rate_derivative,
+            collection_time_s = collection_time_s,
+            speed_cm_s = speed_cm_s
+        ))
+    }
+}
+
+
+predict_count_rate_earth <- function(
+        input_data,
+        photon_energy_keV,
+        yield,
+        activity_Bq = 1,
+        earth_data = earth_model,
+        cpm = FALSE,
+        net_counts_rolling = FALSE,
+        net_counts_cumulative = FALSE,
+        count_rate_derivative = FALSE,
+        collection_time_s = NULL,
+        speed_cm_s = NULL) {
+
+    if (
+        net_counts_rolling
+        | net_counts_cumulative
+        | count_rate_derivative
+        | !is.null(speed_cm_s)
+        | !is.null(collection_time_s)) {
+        warning(paste(
+            "Warning!",
+            "The earth method is currently unable to predict anything besides the net count rate",
+            "The net counts and count rate derivative will not be computed",
+            sep = "\n"
+        ))
+    }
+
+    number_energies <- length(photon_energy_keV)
+
+    if (number_energies != length(yield)){
+        stop(paste(
+            "Error!",
+            "Energy and yield vectors must have same length",
+            sep = "\n"
+        ))
+    }
+
+    # Calculation of net count rate ----
+    data_length <- nrow(input_data)
+
+    output_data <- input_data
+
+    input_data <- input_data[
+        rep(seq_len(nrow(input_data)), number_energies),]
+    input_data$Es_keV <- rep(photon_energy_keV, each = data_length)
+    input_data$yield <- rep(yield, each = data_length)
+
+    input_data <- predict_efficiency(input_data, method = "earth")
+
+    output_data$count_rate <- 0
+    output_data$ugeom_count_rate <- 0
+
+    for (i in 1:data_length) {
+        subset_i <- input_data[
+            output_data$y_cm[i] == input_data$y_cm
+            & output_data$contents[i] == input_data$contents, ]
+
+        output_data$count_rate[i] <- activity_Bq * sum(
+            subset_i$efficiency * subset_i$yield)
+
+        output_data$ugeom_count_rate <- sqrt(sum(
+            (subset_i$ugeom_efficiency * subset_i$yield) ^ 2))
+    }
+
+    return(output_data)
+}
+
+
+predict_count_rate_lm <- function(
         input_data,
         photon_energy_keV,
         yield,
@@ -39,26 +153,28 @@ predict_count_rate <- function(
     if (count_rate_derivative & is.null(speed_cm_s)) {
         stop(paste(
             "Error!",
-            "Derivative of count rate requires truck speed.",
+            "Derivative of count rate requires truck speed",
             sep = "\n"
         ))
     }
     if (net_counts_cumulative & is.null(speed_cm_s)) {
         stop(paste(
             "Error!",
-            "Cumulative net counts requires truck speed.",
+            "Cumulative net counts requires truck speed",
             sep = "\n"
         ))
     }
     if (
-        net_counts_rolling & (is.null(collection_time_s) | is.null(speed_cm_s))
+        net_counts_rolling & (
+            is.null(collection_time_s) | is.null(speed_cm_s))
     ) {
         stop(paste(
             "Error!",
-            "Rolling net counts requiress truck speed and collection time.",
+            "Rolling net counts requiress truck speed and collection time",
             sep = "\n"
         ))
     }
+
 
     # Calculation of net count rate ----
     data_length <- nrow(input_data)
@@ -70,7 +186,7 @@ predict_count_rate <- function(
     input_data$Es_keV <- rep(photon_energy_keV, each = data_length)
     input_data$yield <- rep(yield, each = data_length)
 
-    input_data <- predict_efficiency(input_data)
+    input_data <- predict_efficiency(input_data, method = "lm")
 
     output_data$count_rate <- 0
     output_data$ugeom_count_rate <- 0
@@ -124,7 +240,7 @@ predict_count_rate <- function(
     }
 
     if (cpm) {
-        #
+        print("cpm")
     }
 
     # Return the output data ----
