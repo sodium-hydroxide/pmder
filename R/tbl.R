484ba1affa151e86c7cc0c3dadfde48ce5108dee
sodium-hydroxide <43483259+sodium-hydroxide@users.noreply.github.com>
2024-03-24 11:40:41 -0600

    added materials

--- R/tbl.R
+++ R/tbl.R
@@ -1,46 +1,79 @@
-tbl <- function() {
-    return(list(
-        methods_materials_general =
-            knitr::kable(
-                data.frame(
-                    material =
-                        c(
-                            "Air", "", "", "", "Concrete", "", "", "", "", "", "",
-                            "Earth", "", "", ""
-                        ),
-                    density =
-                        c(
-                            "$1.205 \\times 10^{-3}$", "", "", "", "$1.032$", "",
-                            "", "", "", "", "", "$1.520$", "", "", ""
-                        ),
-                    atom =
-                        c(
-                            "$\\mathrm{C}$", "$\\mathrm{N}$", "$\\mathrm{O}$",
-                            "$\\mathrm{Ar}$", "$\\mathrm{H}$", "$\\mathrm{O}$",
-                            "$\\mathrm{Na}$", "$\\mathrm{Al}$", "$\\mathrm{Si}$",
-                            "$\\mathrm{Ca}$", "$\\mathrm{Fe}$", "$\\mathrm{H}$",
-                            "$\\mathrm{O}$", "$\\mathrm{Al}$", "$\\mathrm{Si}$"
-                        ),
-                    mass_fraction =
-                        c(
-                            "$1.24 \\times 10^{-4}$", "$7.55 \\times 10^{-1}$",
-                            "$2.32 \\times 10^{-1}$", "$1.28 \\times 10^{-2}$",
-                            "$4.53 \\times 10^{-3}$", "$5.13 \\times 10^{-1}$",
-                            "$1.53 \\times 10^{-2}$", "$3.56 \\times 10^{-2}$",
-                            "$3.60 \\times 10^{-1}$", "$5.79 \\times 10^{-2}$",
-                            "$1.38 \\times 10^{-2}$", "$2.38 \\times 10^{-2}$",
-                            "$5.99 \\times 10^{-1}$", "$8.04 \\times 10^{-2}$",
-                            "$2.97 \\times 10^{-1}$"
-                        )
-                ),
-                col.names = c(
-                    "Material",
-                    "Mass Density ($\\mathrm{g}\\,\\mathrm{cm}^{-3}$)",
-                    "Atom", "Mass Fraction"
-                )
-            )
-    ))
-}
-tbl()$methods_materials_general
 
+
+tbl <- function(tbl_name) {
+
+    if (tbl_name == "materials_general") {
+        table <- data.frame(
+            material = c(
+                "Air", "", "", "", "Concrete", "", "", "", "", "", "",
+                "Earth", "", "", ""
+            ),
+            density = c(
+                "$1.205 \\times 10^{-3}$", "", "", "", "$1.032$", "",
+                "", "", "", "", "", "$1.520$", "", "", ""
+            ),
+            atom = c(
+                "$\\mathrm{C}$", "$\\mathrm{N}$", "$\\mathrm{O}$",
+                "$\\mathrm{Ar}$", "$\\mathrm{H}$", "$\\mathrm{O}$",
+                "$\\mathrm{Na}$", "$\\mathrm{Al}$", "$\\mathrm{Si}$",
+                "$\\mathrm{Ca}$", "$\\mathrm{Fe}$", "$\\mathrm{H}$",
+                "$\\mathrm{O}$", "$\\mathrm{Al}$", "$\\mathrm{Si}$"
+            ),
+            mass_fraction = c(
+                "$1.24 \\times 10^{-4}$", "$7.55 \\times 10^{-1}$",
+                "$2.32 \\times 10^{-1}$", "$1.28 \\times 10^{-2}$",
+                "$4.53 \\times 10^{-3}$", "$5.13 \\times 10^{-1}$",
+                "$1.53 \\times 10^{-2}$", "$3.56 \\times 10^{-2}$",
+                "$3.60 \\times 10^{-1}$", "$5.79 \\times 10^{-2}$",
+                "$1.38 \\times 10^{-2}$", "$2.38 \\times 10^{-2}$",
+                "$5.99 \\times 10^{-1}$", "$8.04 \\times 10^{-2}$",
+                "$2.97 \\times 10^{-1}$"
+            )
+        )
+        col_names <- c(
+            "Material", "Mass Density ($\\mathrm{g}\\,\\mathrm{cm}^{-3}$)",
+            "Atom", "Mass Fraction"
+        )
+    }
+
+    else if (tbl_name == "materials_portal_monitor") {
+
+        table <- data.frame(
+            material = c(
+                "Polyvinyl", "Toluene", "Carbon Steel", "", "Lead"
+            ),
+            density = c(
+                "$2.250$", "", "$7.820$", "", "$11.350$"
+            ),
+            atom = c(
+                "$\\mathrm{H}$", "$\\mathrm{C}$", "$\\mathrm{C}$",
+                "$\\mathrm{Fe}$ ", "$\\mathrm{Pb}$ "
+            ),
+            mass_fraction = c(
+                "$8.50 \\times 10^{-2}$", "$9.15 \\times 10^{-1}$",
+                "$5.00 \\times 10^{-3}$", "$9.95 \\times 10^{-1}$", "$1.00$"
+            )
+        )
+
+        col_names <- c(
+            "Material", "Mass Density ($\\mathrm{g}\\,\\mathrm{cm}^{-3}$)",
+            "Atom", "Mass Fraction"
+        )
+    }
+
+    else if (tbl_name == "materials_truck") {
+
+    }
+
+    else {
+        stop(paste(
+            "Potential tables are:",
+            "materials_general",
+            "materials_portal_monitor",
+            sep = "\n"
+        ))
+    }
+
+    return(list(table = table, col_names = col_names))
+}
 
