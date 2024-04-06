4e5f539ba87de8df39260ca1e11e8b7b998394c3
sodium-hydroxide <43483259+sodium-hydroxide@users.noreply.github.com>
2024-04-05 18:53:46 -0600

    Working on model generation

--- data_raw/3_model_creation/1_linear_model.R
+++ data_raw/3_model_creation/1_linear_model.R
@@ -14,8 +14,7 @@ wd <- paste(
 load(paste(
     getwd(),
     "/",
-    "data_raw/2_data_cleaning/",
-    "summary_data.rda",
+    "data/summary_data.rda",
     sep = ""
 ))
 
@@ -25,29 +24,17 @@ linear_model <-
         r_m = (sqrt(y_m^2 + 1.795^2)),
         PrReach = NULL,
         uPrReach = NULL,
-        weight = uPrDet ^ (-2),
+        weight = uPrDet ^ (-2)
     ) |>
     lm(
         log(PrDet) ~ log(r_m) + log(Es_keV) + contents,
-        data = _,
         weights = weight
-    )
-
-linear_model_tidy <-
-    linear_model |>
-    tidy() |>
-    mutate(r.squared = summary(linear_model)$r.squared)
-
-linear_model_aug <- augment(linear_model)
-
-linear_model <- list(
-    model = linear_model,
-    aug = linear_model_aug,
-    tidy = linear_model_tidy
-)
+    ) |>
+    lm_clean()
 
 save(linear_model, file = paste(
     getwd(),
     "/data/linear_model.rda",
     sep = ""
 ))
+
--- data_raw/3_model_creation/2_mars_model.R
+++ data_raw/3_model_creation/2_mars_model.R
@@ -8,6 +8,57 @@ package_load(
 )
 load("data/summary_data.rda")
 
-mars_model_aug
+mars_model_aug <-
+    tibble(
+        `log(PrDet)` = log(summary_data$PrDet),
+        `log(r_m)` = log(sqrt(summary_data$y_m^2 + 1.795^2)),
+        Es_keV = summary_data$Es_keV,
+        contents = summary_data$contents,
+        unc = summary_data$uPrDet
+    ) |>
+    mutate(unc = unc / exp(`log(PrDet)`)) |>
+    mutate(`(weights)` = unc ^ (-2), unc = NULL)
 
+mars_model <-
+    earth(
+        formula = (
+            log(PrDet)
+            ~ log(sqrt(y_m^2 + 1.795^2))
+            + Es_keV
+            + contents
+            + Es_keV:contents
+            + Es_keV:log(sqrt(y_m^2 + 1.795^2))
+        ),
+        data = summary_data,
+        varmod.method = "earth",
+        weights = (PrDet / uPrDet)^2,
+        nfold = 2,
+        ncross = 30
+    )
 
+mars_model_aug <- mutate(mars_model_aug,
+    .fitted = unlist(unname(mars_model$fitted.values)),
+    .resid = unlist(unname(mars_model$residuals)),
+    .hat = hatvalues(mars_model),
+    .sigma = unlist(unname(mars_model$varmod$model.var)),
+    .coodsd = NULL,
+    .std.resid = .resid / .sigma
+)
+
+mars_model_tidy <- tibble(
+    term = rownames(summary(mars_model)$coefficients),
+    estimate = unlist(unname(summary(mars_model)$coefficients)),
+    r.squared = mars_model$rsq
+)
+
+mars_model <-
+    list(model = mars_model, aug = mars_model_aug, tidy = mars_model_tidy)
+
+save(
+    mars_model,
+    file = paste(
+        getwd(),
+        "/data/mars_model.rda",
+        sep = ""
+    )
+)
